1a15bf5ab3e28c8eadff701a847d133ef9bbafcf
may2330 <may2330@naver.com>
2019-07-01 10:38:52 +0900

    img read&show
    python image read color&gray
    python image show

---
+++ imreadEx.py
@@ -0,0 +1,14 @@
+import cv2
+
+void main(){
+	imageFile = './data/lena.jpg'
+	img = cv2.imread(imageFile)
+	img2 = cv2.imread(imageFile,0)
+
+	cv2.imshow('Lena color',img)
+	cv2.imshow('lena grayscale',img2)
+
+	cv2.waitKey()
+	cv2.destroyAllWindows()
+}
+
